50940c3fc1bdd3801271b2ed1e9e2cefb7d97575
TheMecz <maxponcesan@gmail.com>
2019-10-29 14:26:26 -0500

    tarea

---
+++ BuscaMinas/cmake-build-debug/CMakeFiles/BuscaMinas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BuscaMinas.dir/main.cpp.obj"
+  "BuscaMinas.pdb"
+  "BuscaMinas.exe"
+  "BuscaMinas.exe.manifest"
+  "libBuscaMinas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BuscaMinas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
